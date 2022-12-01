f4b45e332e84caf777b2ffca289c0159b5eebb4c
Aaron <aaronrichards4321@gmail.com>
2022-12-01 13:41:49 -0500

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,17 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        
+        var increaseCount = 0
+        var previousInput = input[0]
+        
+        for (x = 1; x < input.Count; x++;) {
+            val currentInput = input[x]
+            if (currentInput > previousInput) {
+                increaseCount++
+            }
+            previousInput = currentInput
+        }
+        return increaseCount
     }
 
     fun part2(input: List<String>): Int {
