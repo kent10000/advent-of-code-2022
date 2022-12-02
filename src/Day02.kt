58e377133ed8550e224e8b31441ec7f3ba356afc
Aaron <aaronrichards4321@gmail.com>
2022-12-02 09:39:05 -0500

    Implemented rock paper scissors logic

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,28 @@
 fun main() {
+    //rock //paper
+    fun rockPaperScissors(player1: Hand, player2: Hand): Int {
+        //Paper Beats Rock
+        //Rock Beats Scissors
+        //Scissors Beat Paper
+        if (player1 == player2) { return 0 } //Draw
+        
+        if ((player1 + 1 == player2) || (player1 == 3 && player2 != Paper)) {
+            return 1
+        }
+        return 2
+    }
 
     fun part1(input: List<String>): Int {
+
+        //Oponent
+        //A - Rock, B - Paper, C - Scissors 
+        //Self
+        //X - Rock 1, Y - Paper 2, Z - Scissors 3
+        //Win is 6 Draw is 3 Loss is 0 
+        
+
+
+
         return 0
     }
 
@@ -18,4 +40,8 @@ fun main() {
     val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
+}
+
+enum class Hand {
+    Rock = 0, Paper, Scissors
 }
