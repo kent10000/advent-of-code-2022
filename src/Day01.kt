4a4547a4ac6d3339e97cf7f030f2a9d5751aa227
kent10000 <aaronrichards4321@gmail.com>
2022-12-01 19:07:00 -0500

    Completed Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,28 +1,29 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        
-        var increaseCount = 0
-        var previousInput: String
-        
-        for (ping: String in input) {
-            
-            val currentInput = ping.toInt()
-            
-            if (currentInput > previousInput) {
-                increaseCount++
+       val elfCalories = mutableListOf(0)
+
+        for (value in input) {
+            if (value.isBlank()) {
+                elfCalories.add(0)
+                continue
             }
-            previousInput = currentInput
+            elfCalories[elfCalories.size - 1] += value.toInt()
         }
-        return increaseCount
+
+        return elfCalories.max()
     }
+
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
+    /* test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 1)
+    */
+
 
     val input = readInput("Day01")
     println(part1(input))
