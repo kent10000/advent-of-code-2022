42a6ca8608a915ba57bb93b16c5a33a67eb6bda2
Aaron <aaronrichards4321@gmail.com>
2022-12-05 18:47:51 -0500

    Update Day05.kt

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,6 +1,41 @@
 fun main() {
+    
+    fun loadStacks(input: List<String>): List<MutableList<Char>> {
+
+        var stacks = input.subList(0, input.indexOf(String()))
+        val num = stacks.last().dropLastWhile { !it.isDigit() }.last().digitToInt()
+        stacks = stacks.dropLast(1)
+        val list: List<MutableList<Char>> = List(num) { mutableListOf()}
+
+        for (line in stacks) {
+            val items = line.chunked(4)
+            //items.forEach { println(it) }
+            for ((cursor, i) in items.withIndex()) {
+                if (i.isNotBlank()) {
+                    list[cursor].add(i[1])
+                }
+            }
+        }
+        list.forEach { it.reverse() }
+        return list
+    }
+
+    fun executeInstruction(instruction: String) {
+        val actions = instruction.split(" ")
+        for (i in actions) {
+            println(i)
+        }
+        //1,2,6
+        //val (count, from, to) = Triple(actions)
+    }
 
     fun part1(input: List<String>): Int {
+
+        val stacks = loadStacks(input)
+        var instructions = input.subList(input.indexOf(String()) + 1, input.size)
+        instructions.forEach { executeInstruction(it); return@forEach}
+        
+        //stacks.forEach { it.forEach { print("$it ")}; println() }
         return 0
     }
 
